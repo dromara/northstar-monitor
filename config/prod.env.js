579a55233a0411a3f1bafb7cd869edda74aa4516
huangwl <huangwl@kamfu.com.cn>
2020-07-12 11:51:28 +0800

    更新nginx转发配置

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  SOCKET_ENDPOINT: '"http://47.100.162.95:51888"',
-  TRADER_ENDPOINT: '"http://47.100.162.95:8888"',
+  SOCKET_ENDPOINT: '""',
+  TRADER_ENDPOINT: '"/trader"',
   STRATEGY_ENDPOINT: '""'
 }
