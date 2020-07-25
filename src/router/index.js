422334bc34e0bbf85a40024d7e3c1af2f130074b
huangwl <huangwl@kamfu.com.cn>
2020-07-25 17:57:44 +0800

    增加路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../pages/Login'
 import Monitor from '../pages/Monitor'
+import loginService from '../api/service/loginService'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
       path: '/',
@@ -15,7 +16,32 @@ export default new Router({
     {
       path: '/monitor',
       name: 'monitor',
+      meta: {
+        requireAuth: true
+      },
       component: Monitor
     }
   ]
 })
+
+// 配置路由权限
+router.beforeEach(async (to, from, next) => {
+  if (to.meta.requireAuth) { // 判断该路由是否需要登录权限
+    let auth
+    try {
+      auth = await loginService.auth()
+    } catch (e) {
+      console.log(e)
+    }
+    if (auth) { // 判断本地是否存在access_token
+      next()
+    } else {
+      // 未登录,跳转到登陆页面，并且带上 将要去的地址，方便登陆后跳转。
+      next('/')
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
