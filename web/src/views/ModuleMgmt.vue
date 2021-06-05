e038b80de70d8980c49c59a57fb6bc294cbca02f
KevinHuangwl <kevinbauer@163.com>
2021-06-05 15:27:04 +0800

    完成列表页

--- web/src/views/ModuleMgmt.vue
+++ web/src/views/ModuleMgmt.vue
@@ -3,15 +3,56 @@
     <CtaForm
       :visible.sync="ctaModuleFormVisible"
       :isUpdateMode="curTableIndex > -1"
+      :module="curModule"
       @onSave="onSave"
     />
-    <el-table height="100%">
-      <el-table-column label="模组名称" align="center" />
-      <el-table-column label="绑定账户" align="center" />
-      <el-table-column label="信号策略" align="center" />
-      <el-table-column label="风控策略" align="center" />
-      <el-table-column label="交易策略" align="center" />
-      <el-table-column align="center" width="300px">
+    <el-table height="100%" :data="list">
+      <el-table-column label="模组名称" prop="moduleName" align="center" />
+      <el-table-column
+        label="绑定账户"
+        prop="accountGatewayId"
+        align="center"
+      />
+      <el-table-column
+        label="资金占用上限"
+        prop="allocatedAccountShare"
+        align="center"
+        width="80px"
+      >
+        <template slot-scope="scope"
+          >{{ scope.row.allocatedAccountShare }}%</template
+        >
+      </el-table-column>
+      <el-table-column
+        label="信号策略"
+        prop="signalPolicy.componentMeta.name"
+        align="center"
+      />
+      <el-table-column label="风控策略" align="center">
+        <template slot-scope="scope">
+          {{
+            scope.row.riskControlRules
+              .map((i) => i.componentMeta.name)
+              .join(',')
+          }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        label="交易策略"
+        prop="dealer.componentMeta.name"
+        align="center"
+      />
+      <el-table-column
+        label="是否启用"
+        prop="enabled"
+        align="center"
+        width="60px"
+      >
+        <template slot-scope="scope">{{
+          scope.row.enabled ? '启用' : '停用'
+        }}</template>
+      </el-table-column>
+      <el-table-column align="center" width="150px">
         <template slot="header">
           <el-button size="mini" type="primary" @click="handleCreate"
             >新建</el-button
@@ -45,8 +86,13 @@ export default {
   data() {
     return {
       ctaModuleFormVisible: false,
-      curTableIndex: -1
+      curTableIndex: -1,
+      curModule: {},
+      list: []
     }
+  },
+  mounted() {
+    this.findAll()
   },
   methods: {
     handleCreate() {
@@ -55,9 +101,14 @@ export default {
     },
     handleEdit(index, row) {
       console.log(index, row)
+      this.curTableIndex = index
+      this.curModule = row
+      this.ctaModuleFormVisible = true
     },
-    handleDelete(index, row) {
+    async handleDelete(index, row) {
       console.log(index, row)
+      await ctaModuleApi.removeModule(row.moduleName)
+      this.findAll()
     },
     async onSave(obj) {
       console.log(obj)
@@ -66,8 +117,11 @@ export default {
       } else {
         await ctaModuleApi.updateModule(obj)
       }
-      let result = await ctaModuleApi.getAllModules()
-      console.log(result)
+      this.findAll()
+    },
+    async findAll() {
+      this.list = await ctaModuleApi.getAllModules()
+      console.log(this.list)
     }
   }
 }
