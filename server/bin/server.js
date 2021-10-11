15cb51893bce4fc8fcec27abe25c2c4ca107a974
KevinHuangwl <kevinbauer@163.com>
2021-10-11 23:17:58 +0800

    修复前端服务器没有统一处理异常导致程序崩溃问题

--- server/bin/server.js
+++ server/bin/server.js
@@ -63,11 +63,13 @@ function onError(error) {
     case 'EACCES':
       console.error(bind + ' requires elevated privileges')
       process.exit(1)
+      break
     case 'EDDRINUSE':
       console.error(bind + ' is already in use')
       process.exit(1)
+      break
     default:
-      throwerror
+      throw error
   }
 }
 
@@ -84,3 +86,8 @@ function onListening() {
       ? 'pipe ' + addr
       : 'port ' + addr.port
 }
+
+process.on('uncaughtException', (err) => {
+  console.error(err)
+  console.error(err.stack)
+})
