fb9ad8e5c43a4e3f3f5930764b41fe67aab22bc6
KevinHuangwl <kevinbauer@163.com>
2022-01-02 14:19:12 +0800

    简化网关配置信息

--- src/components/CtpForm.vue
+++ src/components/CtpForm.vue
@@ -1,6 +1,6 @@
 <template>
   <el-dialog
-    width="768px"
+    width="500px"
     title="CTP网关配置"
     :visible.sync="dialogVisible"
     append-to-body
@@ -16,77 +16,18 @@
       :rules="formRules"
     >
       <el-row>
-        <el-col :span="8">
+        <el-col :span="12">
           <el-form-item label="网关账户" prop="userId">
-            <el-input
-              v-model="ctpSettings.userId"
-              autocomplete="off"
-            ></el-input>
+            <el-input v-model="ctpSettings.userId" autocomplete="off"></el-input>
           </el-form-item>
         </el-col>
-        <el-col :span="8">
+        <el-col :span="12">
           <el-form-item label="网关密码" prop="password">
             <el-input
               v-model="ctpSettings.password"
               type="password"
               autocomplete="off"
               show-password
-            ></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="8">
-          <el-form-item
-            label="期货公司编码"
-            label-width="130px"
-            prop="brokerId"
-          >
-            <el-input
-              v-model="ctpSettings.brokerId"
-              autocomplete="off"
-            ></el-input>
-          </el-form-item>
-        </el-col>
-      </el-row>
-
-      <el-row>
-        <el-col :span="8">
-          <el-form-item label="网关IP" prop="host">
-            <el-input v-model="ctpSettings.host" autocomplete="off"></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="6">
-          <el-form-item label="网关端口" prop="port">
-            <el-input
-              v-model="ctpSettings.port"
-              autocomplete="off"
-              type="number"
-            ></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="10">
-          <el-form-item label="授权码" prop="authCode">
-            <el-input
-              v-model="ctpSettings.authCode"
-              autocomplete="off"
-            ></el-input>
-          </el-form-item>
-        </el-col>
-      </el-row>
-      <el-row>
-        <el-col :span="11">
-          <el-form-item label="AppID" prop="appId">
-            <el-input v-model="ctpSettings.appId" autocomplete="off"></el-input>
-          </el-form-item>
-        </el-col>
-        <el-col :span="13">
-          <el-form-item
-            label="UserProductInfo"
-            label-width="150px"
-            prop="userProductInfo"
-          >
-            <el-input
-              v-model="ctpSettings.userProductInfo"
-              autocomplete="off"
             ></el-input>
           </el-form-item>
         </el-col>
@@ -119,26 +60,14 @@ export default {
     return {
       formRules: {
         userId: [{ required: true, message: '不能为空', trigger: 'blur' }],
-        password: [{ required: true, message: '不能为空', trigger: 'blur' }],
-        brokerId: [{ required: true, message: '不能为空', trigger: 'blur' }],
-        host: [{ required: true, message: '不能为空', trigger: 'blur' }],
-        port: [{ required: true, message: '不能为空', trigger: 'blur' }],
-        authCode: [{ required: true, message: '不能为空', trigger: 'blur' }],
-        userProductInfo: [
-          { required: true, message: '不能为空', trigger: 'blur' }
-        ],
-        appId: [{ required: true, message: '不能为空', trigger: 'blur' }]
+        password: [{ required: true, message: '不能为空', trigger: 'blur' }]
+        // brokerId: [{ required: true, message: '不能为空', trigger: 'blur' }],
       },
       dialogVisible: false,
       ctpSettings: {
         userId: '',
-        password: '',
-        brokerId: '',
-        host: '',
-        port: '',
-        authCode: '',
-        userProductInfo: '',
-        appId: ''
+        password: ''
+        // brokerId: '',
       }
     }
   },
@@ -150,10 +79,6 @@ export default {
           return
         }
         Object.assign(this.ctpSettings, this.ctpSettingsSrc)
-        this.ctpSettings.host =
-          this.ctpSettingsSrc.mdHost || this.ctpSettingsSrc.tdHost
-        this.ctpSettings.port =
-          this.ctpSettingsSrc.mdPort || this.ctpSettingsSrc.tdPort
       }
     },
     dialogVisible: function (val) {
@@ -168,10 +93,6 @@ export default {
       this.ctpSettings = this.$options.data().ctpSettings
     },
     saveCtpSetting() {
-      this.ctpSettings.mdHost = this.ctpSettings.host
-      this.ctpSettings.mdPort = this.ctpSettings.port
-      this.ctpSettings.tdHost = this.ctpSettings.host
-      this.ctpSettings.tdPort = this.ctpSettings.port
       this.$refs.ctpSettings.validate((valid) => {
         if (valid) {
           let obj = {}
